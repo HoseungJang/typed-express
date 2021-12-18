30bc37c4efba1a038c66b67999855d2fb680997d
HoseungJang <hsjang.dev@gmail.com>
2021-12-19 06:42:34 +0900

    fix import path

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export * from "request-typer";
 export * from "./application";
 export * from "./switch";
 export * from "./route";
-export * from "./middleWare";
+export * from "./middleware";
 export * from "./errorHandler";
 export * from "./errorResponse";
 export * from "./openAPIRoute";
